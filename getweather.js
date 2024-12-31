f05d0f2b72b3bb1231d376f72a6506bdaf29ad99
abc <abc@gmail.com>
2024-12-31 11:33:35 +0800

    feat:init

---
+++ getweather.js
@@ -0,0 +1,30 @@
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+//定义返回的 html 结构
+const html = `
+<div id="he-plugin-standard"></div>
+<script>
+WIDGET = {
+  "CONFIG": {
+    "layout": "2",
+    "width": 230,
+    "height": 270,
+    "background": "1",
+    "dataColor": "FFFFFF",
+    "key": "这里是和风天气的key"
+  }
+}
+</script>
+<script src="https://widget.qweather.net/standard/static/js/he-standard-common.js?v=2.0"></script>
+`
+
+async function handleRequest(request) {
+  return new Response(html, {
+      //设置响应头
+    headers: {
+      "content-type": "text/html;charset=UTF-8",
+    },
+  })
+}
